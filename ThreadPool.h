78c265e2455e8b9d1effb5bc6adae3ceea838464
ZashJie <1030298236@qq.com>
2024-03-24 16:46:42 +0800

    personalStudy

--- ThreadPool.h
+++ ThreadPool.h
@@ -1,98 +1,101 @@
-#ifndef THREAD_POOL_H
-#define THREAD_POOL_H
+#include <iostream>
 
 #include <vector>
 #include <queue>
-#include <memory>
-#include <thread>
 #include <mutex>
-#include <condition_variable>
+// #include <memory>
+#include <thread>
 #include <future>
 #include <functional>
+#include <condition_variable>
 #include <stdexcept>
+
 
 class ThreadPool {
 public:
     ThreadPool(size_t);
+
     template<class F, class... Args>
-    auto enqueue(F&& f, Args&&... args) 
+    auto enqueue(F &&f, Args&&... args)
         -> std::future<typename std::result_of<F(Args...)>::type>;
+
     ~ThreadPool();
+
 private:
-    // need to keep track of threads so we can join them
-    std::vector< std::thread > workers;
-    // the task queue
-    std::queue< std::function<void()> > tasks;
-    
-    // synchronization
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+
     std::mutex queue_mutex;
     std::condition_variable condition;
     bool stop;
 };
- 
-// the constructor just launches some amount of workers
-inline ThreadPool::ThreadPool(size_t threads)
-    :   stop(false)
-{
-    for(size_t i = 0;i<threads;++i)
+
+// 线程池构造函数，通过 for 循环将获取任务的函数塞入 workers 中，顺便创建四个线程不断执行着这个函数。
+ThreadPool::ThreadPool(size_t threads)
+ : stop(false) {
+    for (int i = 0; i < threads; i++) {
         workers.emplace_back(
             [this]
             {
-                for(;;)
-                {
+                for(;;) {
                     std::function<void()> task;
-
                     {
                         std::unique_lock<std::mutex> lock(this->queue_mutex);
-                        this->condition.wait(lock,
-                            [this]{ return this->stop || !this->tasks.empty(); });
-                        if(this->stop && this->tasks.empty())
+
+                        // 条件变量等待，如果 stop为ture 或者 tasks为空 则进去等待
+                        this->condition.wait(lock, 
+                            [this]{return this->stop || !this->tasks.empty();});
+
+                        // 如果 stop为ture 且 tasks为空时，退出函数
+                        if (this->stop && this->tasks.empty()) {
                             return;
+                        }
                         task = std::move(this->tasks.front());
                         this->tasks.pop();
                     }
-
                     task();
                 }
             }
         );
+    }
 }
 
-// add new work item to the pool
+// 使用模版操作，较为复杂
+// 将要线程执行的任务塞入 tasks 中
 template<class F, class... Args>
-auto ThreadPool::enqueue(F&& f, Args&&... args) 
-    -> std::future<typename std::result_of<F(Args...)>::type>
+auto ThreadPool::enqueue(F&& f, Args&&... args)
+    -> std::future<typename std::result_of<F(Args...)>::type> 
 {
     using return_type = typename std::result_of<F(Args...)>::type;
 
     auto task = std::make_shared< std::packaged_task<return_type()> >(
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-        );
-        
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+    );
+
+    // 给将来函数的返回值做准备
     std::future<return_type> res = task->get_future();
     {
-        std::unique_lock<std::mutex> lock(queue_mutex);
+        // 在该作用域中，加锁
+        std::unique_lock<std::mutex> lock(this->queue_mutex);
 
-        // don't allow enqueueing after stopping the pool
-        if(stop)
-            throw std::runtime_error("enqueue on stopped ThreadPool");
-
-        tasks.emplace([task](){ (*task)(); });
+        if (stop) {
+            throw std::runtime_error("enqueue on stopped ThreadPool.");
+        }
+        tasks.emplace([task]() { (*task)(); });
     }
     condition.notify_one();
     return res;
 }
 
-// the destructor joins all threads
-inline ThreadPool::~ThreadPool()
-{
+inline ThreadPool::~ThreadPool() {
     {
-        std::unique_lock<std::mutex> lock(queue_mutex);
+        std::unique_lock<std::mutex> lock(this->queue_mutex);
         stop = true;
     }
-    condition.notify_all();
-    for(std::thread &worker: workers)
-        worker.join();
-}
+    this->condition.notify_all();
 
-#endif
+    // 结束函数
+    for (std::thread& worker : workers) {
+        worker.join();
+    }
+}
--- example.cpp
+++ example.cpp
@@ -1,18 +1,16 @@
-#include <iostream>
-#include <vector>
-#include <chrono>
+#include "studyPool.h"
+// #include <iostream>
+using namespace std;
 
-#include "ThreadPool.h"
-
-int main()
-{
-    
+int main() {
     ThreadPool pool(4);
-    std::vector< std::future<int> > results;
-
-    for(int i = 0; i < 8; ++i) {
+    // 通过将线程池函数返回的值存放到 std::future<int> 的集合中
+    std::vector<std::future<int>> results;
+    
+    for (int i = 0; i < 8; i++) {
+        // emplace_back 的元素实际上是函数的返回值
         results.emplace_back(
-            pool.enqueue([i] {
+            pool.enqueue([i](){
                 std::cout << "hello " << i << std::endl;
                 std::this_thread::sleep_for(std::chrono::seconds(1));
                 std::cout << "world " << i << std::endl;
@@ -21,9 +19,10 @@ int main()
         );
     }
 
-    for(auto && result: results)
-        std::cout << result.get() << ' ';
-    std::cout << std::endl;
-    
-    return 0;
+    // 通过循环将存放进入的返回值元素打印出来
+    for (auto && result : results) {
+        cout << result.get() << endl;
+        cout << "===========" << endl;
+    }
+
 }
